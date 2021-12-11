cb69ae2cd2f14ef04a1a6daf596ff7efdf298675
Marissa Hughes <hughes.marissa331@gmail.com>
2021-12-11 14:38:09 -0800

    Add/switch content to app.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Analog Clocl</header>
+      <div className="clock">
+        <div className="hand hour"></div>
+        <div className="hand minute"></div>
+        <div className="hand second"></div>
+        <div className="number number1">1</div>
+        <div className="number number2">2</div>
+        <div className="number number3">3</div>
+        <div className="number number4">4</div>
+        <div className="number number5">5</div>
+        <div className="number number6">6</div>
+        <div className="number number7">7</div>
+        <div className="number number8">8</div>
+        <div className="number number9">9</div>
+        <div className="number number10">10</div>
+        <div className="number number11">11</div>
+        <div className="number number12">12</div>
+    </div>
     </div>
   );
 }
