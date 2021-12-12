b089369d3b96ea011878ad2de136a008923a0523
Marissa Hughes <hughes.marissa331@gmail.com>
2021-12-11 16:51:45 -0800

    Add upright numbers

--- src/App.js
+++ src/App.js
@@ -2,26 +2,25 @@ import React from 'react';
 import './App.css';
 import ClockHands from './ClockHands';
 
-
 function App() {
   return (
     <div className="App">
         <div className="clock">
-                <div className="hand hour" data-hour-hand></div>
-                <div className="hand minute" data-minute-hand></div>
-                <div className="hand second" data-second-hand></div>
-                <div className="number number1">1</div>
-                <div className="number number2">2</div>
-                <div className="number number3">3</div>
-                <div className="number number4">4</div>
-                <div className="number number5">5</div>
-                <div className="number number6">6</div>
-                <div className="number number7">7</div>
-                <div className="number number8">8</div>
-                <div className="number number9">9</div>
-                <div className="number number10">10</div>
-                <div className="number number11">11</div>
-                <div className="number number12">12</div>
+                <div className="hand hour reverse" data-hour-hand></div>
+                <div className="hand minute reverse" data-minute-hand></div>
+                <div className="hand second reverse" data-second-hand></div>
+                <div className="number number1"><div className="reverse">1</div></div>
+                <div className="number number2"><div className="reverse">2</div></div>
+                <div className="number number3"><div className="reverse">3</div></div>
+                <div className="number number4"><div className="reverse">4</div></div>
+                <div className="number number5"><div className="reverse">5</div></div>
+                <div className="number number6"><div className="reverse">6</div></div>
+                <div className="number number7"><div className="reverse">7</div></div>
+                <div className="number number8"><div className="reverse">8</div></div>
+                <div className="number number9"><div className="reverse">9</div></div>
+                <div className="number number10"><div className="reverse">10</div></div>
+                <div className="number number11"><div className="reverse">11</div></div>
+                <div className="number number12"><div className="reverse">12</div></div>
         </div>
         <footer className="App-footer">Analog Clock</footer>
     </div>
