974dceddbda193f1579aaf32ec920814904f70bf
Marissa Hughes <hughes.marissa331@gmail.com>
2021-12-12 09:06:20 -0800

    Instll gh-pgs

--- src/ClockHands.js
+++ src/ClockHands.js
@@ -5,8 +5,6 @@ const minuteHand = document.querySelector('[data-minute-hand]');
 const secondHand = document.querySelector('[data-second-hand]');
 
 function setClock() {
-    
-
     const currentDate = new Date();
     const secondsRatio = currentDate.getSeconds() / 60;
     const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
@@ -18,7 +16,6 @@ function setClock() {
 }
 
 function setRotation(element, rotationRatio) {
-    //element.style.setProperty('--rotation', rotationRatio * 360);
+   element.style.setProperty('--rotation', rotationRatio * 360);
 } 
 setClock();
-export default setClock;
